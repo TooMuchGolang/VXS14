badc2b81f3d9e059416dccb5f24dfdb25ddc6825
TooMuchGolang <zendr32@gmail.com>
2023-11-19 15:22:48 +0900

    Revert "Update VendingMachineComponent.cs"
    This reverts commit ca2fe026a93dbae189f4fc43c7d5f41a7278e4fd.

--- Content.Shared/VendingMachines/VendingMachineComponent.cs
+++ Content.Shared/VendingMachines/VendingMachineComponent.cs
@@ -2,7 +2,6 @@ using Content.Shared.Actions;
 using Content.Shared.Whitelist;
 using Robust.Shared.Audio;
 using Robust.Shared.Containers;
-using Content.Shared.Actions.ActionTypes;
 using Robust.Shared.GameStates;
 using Robust.Shared.Serialization;
 using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom;
