12293372c7c899b60807bf64a668e5b6a1c39b56
TooMuchGolang <zendr32@gmail.com>
2024-01-27 01:58:47 +0900

    Update VendingMachineComponent.cs

--- Content.Shared/VendingMachines/VendingMachineComponent.cs
+++ Content.Shared/VendingMachines/VendingMachineComponent.cs
@@ -2,7 +2,6 @@ using Content.Shared.Actions;
 using Content.Shared.Whitelist;
 using Robust.Shared.Audio;
 using Robust.Shared.Containers;
-using Content.Shared.Actions.ActionTypes;
 using Robust.Shared.GameStates;
 using Robust.Shared.Prototypes;
 using Robust.Shared.Serialization;
